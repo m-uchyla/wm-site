0bf0faeb23c04f39f0657b26a7fe01aff5bc3ea1
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-27 15:00:27 +0200

    Button variant

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -1,9 +1,9 @@
 
 
-export default function Button({text, className}) {
+export default function Button({text, variation}) {
     return (
-        <div className={`bg-blue-gradient grow px-6 rounded-sm cursor-pointer flex-center h-[45px] ${className}`}>
-            <button className={`sm:text-[20px] text-[10px] text-background font-semibold`}>{text}</button>
-        </div>
+        <div className={`${(variation === 1) ? "bg-blue-gradient text-background " : "border-2 border-gradient text-primary" } grow px-6 cursor-pointer flex-center h-[45px]`}>
+            <button className={`sm:text-[20px] text-[10px] font-semibold`}>{text}</button>
+        </div>  
     )
 }
--- src/components/templates/main.js
+++ src/components/templates/main.js
@@ -1,9 +1,15 @@
-import { About, Contact, Hero, Pricing } from '../index'
+import { About, Contact, Hero, Pricing, Button } from '../index'
 
 export default function Main() {
     return (
       <>
         <Hero />
+        <div className='w-[200px] p-10'>
+        <Button text={"Click"} variation={1} />
+        </div>
+        <div className='w-[200px] p-10'>
+        <Button text={"Click"} variation={2} />
+        </div>
         <About />
         <Pricing />
         <Contact />
