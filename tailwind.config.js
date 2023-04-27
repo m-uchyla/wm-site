620e54fe3d9d1123adae41def859802f5dd085ee
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-27 12:34:20 +0200

    Initial styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#00d8ff",
+        secondary: "#5eead4",
+        background:"#05041b",
+        primaryText:"#ffffff",
+        secondaryText:"#9ca3af",
       },
+    },
+    screens: {
+      xxs: "330px",
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
   plugins: [],
