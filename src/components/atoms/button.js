ec3b442833bdad2a4f7d63006d4fd3df85eb05eb
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-27 13:18:11 +0200

    Button

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -1,9 +1,9 @@
 
 
-export default function Button() {
+export default function Button({text, className}) {
     return (
-      <div>
-        Button
-      </div>
+        <div className={`bg-blue-gradient grow px-6 rounded-sm cursor-pointer flex-center ${className}`}>
+            <button className={`sm:text-[20px] text-[10px] text-background font-semibold`}>{text}</button>
+        </div>
     )
 }
