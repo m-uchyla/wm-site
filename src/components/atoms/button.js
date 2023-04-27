9f1b4ae34308be6f23cfd7b7e36e09850e626ccb
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-04-27 13:20:39 +0200

    Button height

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -2,7 +2,7 @@
 
 export default function Button({text, className}) {
     return (
-        <div className={`bg-blue-gradient grow px-6 rounded-sm cursor-pointer flex-center ${className}`}>
+        <div className={`bg-blue-gradient grow px-6 rounded-sm cursor-pointer flex-center h-[45px] ${className}`}>
             <button className={`sm:text-[20px] text-[10px] text-background font-semibold`}>{text}</button>
         </div>
     )
