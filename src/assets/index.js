85fba33ef23954605c2c73b6d619249bd3b85326
witekZWitkowic <witoldadamus94@gmail.com>
2023-04-28 12:10:17 +0200

    Navbar and logos

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import favicon from "./logoImgs/favicon.png"
+import WMlogo from "./logoImgs/WMlogo.png"
+
+export {
+    favicon,
+    WMlogo
+}
--- src/components/templates/navbar.js
+++ src/components/templates/navbar.js
@@ -1,16 +1,33 @@
+import { WMlogo } from "../../assets"
 import { navLinks } from "@/constants"
+import Image from "next/image"
+import { useState } from "react";
 
 export default function Navbar() {
-    return (
-      <nav className="flex justify-between">
-        <div>
+  const [navbar, setNavbar] = useState(false);
 
-        </div>
-        <ul className="flex">
-          {navLinks.map((link, index) => (
-            <li className={`text-[10px] ${index === navLinks.length - 1 ? "mr-0" : "mr-2"}`}>{link.title}</li>
-          ))}
-        </ul>
-      </nav>
-    )
+  const changeBackground = () => {
+    if(window.scrollY >= 5) {
+      setNavbar(true)
+    } else {
+      setNavbar(false)
+    }
+  }
+  window.addEventListener('scroll', changeBackground);
+
+  return (
+    <nav className={`flex justify-between py-1 ${navbar ? "navbar-down" : "navbar-up"}`}>
+      <div>
+        <Image 
+        src={WMlogo}
+        width={30}
+        />
+      </div>
+      <ul className="flex">
+        {navLinks.map((link, index) => (
+          <li className={`text-[10px] ${index === navLinks.length - 1 ? "mr-0" : "mr-2"} cursor-pointer`}>{link.title}</li>
+        ))}
+      </ul>
+    </nav>
+  )
   }
