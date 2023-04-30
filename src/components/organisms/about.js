04868af9ab506822d6829747112170e15a0ddd97
witekZWitkowic <witoldadamus94@gmail.com>
2023-05-01 00:41:30 +0200

    Farmer motion added (install farmer motion and icons!)

--- src/components/organisms/about.js
+++ src/components/organisms/about.js
@@ -1,8 +1,23 @@
 import { about } from "@/constants";
-import Image from "next/image";
-
+import { motion, useScroll } from "framer-motion"
 
 export default function About() {
+  const { scrollYProgress } = useScroll();
+  const variants = {
+    offscreen: {
+      y: 300
+    },
+    onscreen: {
+      y: 50,
+      rotate: -10,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8
+      }
+    }
+  };
+  
     return (
       <section>
         <h2 className='text-primaryText md:text-[58px] text-[54px] font-semibold mb-3 text-center'>
@@ -11,13 +26,20 @@ export default function About() {
         </h2>
         <div className="flex justify-between pt-16">
         {about.map((elem, index) => (
-          <div key={index} className="max-w-[325px] flex flex-col items-center ">
+          <motion.div 
+            key={index} 
+            className="max-w-[325px] flex flex-col items-center"
+            initial={{ opacity: 0, y: 150 }}
+            whileInView={{ opacity: 1, y: 50, }}
+            viewport={{ once: true, amount: 0.1 }}
+            transition={{ delay: index === 0 ? 0.1 : index === 1 ? 0.3 : 0.5 }}
+          >
             <div className="bg-blue-gradient w-[60px] h-[60px] opacity-75 rounded-full flex justify-center items-center">
               <elem.img size={40} className="text-background"/>
             </div>
             <h2 className="text-[24px] font-bold">{elem.title}</h2>
             <p className="text-[16px] text-secondaryText text-center">{elem.description}</p>
-          </div>
+          </motion.div>
         ))}
         </div>
       </section>
