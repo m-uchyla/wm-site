8950ae3977a630a5646a75661cb454b0fcbd9371
witekZWitkowic <witoldadamus94@gmail.com>
2023-05-01 16:23:34 +0200

    About

--- src/components/organisms/about.js
+++ src/components/organisms/about.js
@@ -3,31 +3,17 @@ import { motion, useScroll } from "framer-motion"
 
 export default function About() {
   const { scrollYProgress } = useScroll();
-  const variants = {
-    offscreen: {
-      y: 300
-    },
-    onscreen: {
-      y: 50,
-      rotate: -10,
-      transition: {
-        type: "spring",
-        bounce: 0.4,
-        duration: 0.8
-      }
-    }
-  };
   
     return (
       <section>
-        <h2 className='text-primaryText md:text-[58px] text-[54px] font-semibold mb-3 text-center'>
+        <h2 className='text-primaryText md:text-[48px] text-[54px] font-semibold mb-3 text-center'>
           Ty prowadź swój biznes, <br className='sm:block hidden'/> {" "}
           My zajmiemy się stroną.
         </h2>
         <div className="flex justify-between pt-16">
         {about.map((elem, index) => (
           <motion.div 
-            key={index} 
+            key={index}
             className="max-w-[325px] flex flex-col items-center"
             initial={{ opacity: 0, x: -150 }}
             whileInView={{ opacity: 1, x: 0, }}
--- src/components/organisms/hero.js
+++ src/components/organisms/hero.js
@@ -5,7 +5,7 @@ import Image from "next/image"
 export default function Hero() {
     return (
       <section>
-        <div className="flex justify-between items-center gap-10">
+        <div className="flex justify-between items-center gap-10 pt-20">
           <div className="flex-1">
             <h1 className="text-[70px] font-bold leading-none">Pomożemy Ci być <span className="text-gradient">widocznym</span> w internecie</h1>
             <h2 className="text-secondaryText text-[20px] my-5">Web-Masters zapewni twojej firmie profesjonalną stronę internetową wykonaną z użyciem najnowszych technologii.</h2>
