d8eec750f1bc8b03ab35dbd9d3e3da0a31c3ccdd
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-05-02 15:25:12 +0200

    Footer

--- src/components/templates/footer.js
+++ src/components/templates/footer.js
@@ -2,8 +2,18 @@
 
 export default function Footer() {
     return (
-      <footer>
-        Footer
+      <footer className="text-secondaryText">
+        <div className="bg-[rgba(255,255,255,0.15)] h-[1px] mb-6" />
+        <div className="flex justify-between">
+          <div>
+            © {new Date().getFullYear()} Web-Masters.pl
+          </div>
+          <p>Web-Masters nowoczesne strony internetowe dla biznesu</p>
+          <ul className="flex gap-7">
+            <li>Cookies</li>
+            <li>Polityka prywatności</li>
+          </ul>
+        </div>
       </footer>
     )
   }
