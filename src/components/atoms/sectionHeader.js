d40226a9ca6cb71e7d4bc201348022c5ba7f5e49
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-05-02 12:58:12 +0200

    Responsive pricing

--- src/components/atoms/sectionHeader.js
+++ src/components/atoms/sectionHeader.js
@@ -4,8 +4,8 @@ export default function SectionHeader({header, subheader}) {
 
     return (
       <div className="text-center mb-16">
-          <h2 className="text-[50px] font-semibold">{header}</h2>
-          <h3 className="text-secondaryText text-[20px] font-regular">{subheader}</h3>
+          <h2 className="md:text-[50px] text-[32px] font-semibold">{header}</h2>
+          <h3 className="text-secondaryText md:text-[20px] text-[18px] font-regular">{subheader}</h3>
       </div>
     )
 }
--- src/components/organisms/pricing.js
+++ src/components/organisms/pricing.js
@@ -8,25 +8,27 @@ export default function Pricing() {
     let elements = document.getElementsByClassName("feature"+index);
     for(let i = 0; i<elements.length; i++){
       elements[i].style.color = (isHovered ? "white" : "");
+      elements[i].style.scale = (isHovered ? "1.1" : "");
+      elements[i].style.transitionDuration  = "0.5s";
     }
   }
 
     return (
       <section>
         <SectionHeader header={"Poznaj naszą ofertę"} subheader={"Potrzebujesz strony napisanej od zera? Wybierz coś dla siebie"} />
-        <div className="flex justify-between gap-5">
+        <div className="flex lg:flex-row flex-col justify-between gap-5">
           {pricing.map((plan, index) => (
             <Card className={(index==1 ? "border-2 border-primary" : "")}>
-              <h4 className="text-[24px] font-semibold h-[40px]">{plan.title}</h4>
-              <p className="text-secondaryText h-[80px]">{plan.desc}</p>
-              <div className="h-[60px]">
+              <h4 className="text-[24px] font-semibold xl:h-[40px] h-auto">{plan.title}</h4>
+              <p className="text-secondaryText xl:h-[80px]">{plan.desc}</p>
+              <div className="h-[60px] md:m-0 mt-4">
                 {(plan.discount == 0) ? 
-                  <h5 className='text-[30px] font-semibold'><span className="text-secondaryText font-normal text-[20px]">od</span> {plan.price} zł <span className="text-secondaryText font-normal text-[20px]">netto</span></h5> 
+                  <h5 className='xl:text-[30px] text-[25px] font-semibold'><span className="text-secondaryText font-normal xl:text-[20px] text-[16px]">od</span> {plan.price} zł <span className="text-secondaryText font-normal text-[20px]">netto</span></h5> 
                   : 
-                  <h5 className='text-[30px] font-semibold'><span className="text-secondaryText font-normal text-[20px]">od</span> <span className='text-cyan-500 line-through '><span className='text-zinc-500'>{plan.price}</span></span> {Math.floor(plan.price * (1-(plan.discount/100)))} zł <span className="text-secondaryText font-normal text-[20px]">netto</span></h5>
+                  <h5 className='xl:text-[30px] text-[25px] font-semibold'><span className="text-secondaryText font-normal xl:text-[20px] text-[16px]">od</span> <span className='text-cyan-500 line-through '><span className='text-zinc-500'>{plan.price}</span></span> {Math.floor(plan.price * (1-(plan.discount/100)))} zł <span className="text-secondaryText font-normal text-[20px]">netto</span></h5>
                 }
               </div>
-              <div className="mb-4">
+              <div className="mb-5">
                 <Button text="Wybierz" variation={1} />
               </div>
               <div>
