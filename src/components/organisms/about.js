ae36339aac11b526dbf1bd6f29912d2881091284
mateusz uchyla <45610643+m-uchyla@users.noreply.github.com>
2023-05-03 16:30:46 +0200

    about primary rwd

--- src/components/organisms/about.js
+++ src/components/organisms/about.js
@@ -9,8 +9,8 @@ export default function About() {
     return (
       <section id="about">
         <SectionHeader header={"Parę słów o nas"} subheader={"Zobacz co nas wyróżnia"} />
-        <div className="flex gap-16">
-          <div className="flex-1">
+        <div className="flex flex-col xl:flex-row gap-16">
+          <div className="flex-1 md:block hidden mx-auto">
             <Image 
               src={aboutImg}
               alt="Edytor kodu"
@@ -20,7 +20,7 @@ export default function About() {
           </div>
           <div className="flex-1 flex-center mt-8">
             {/* <div className="grid grid-cols-2 grid-rows-2 gap-5 items-stretch"> */}
-            <div className="grid grid-cols-2 gap-10 items-stretch">
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-10 items-stretch">
               {about.map((elem, index) => (
                 <motion.div 
                   key={index}
@@ -29,7 +29,7 @@ export default function About() {
                   viewport={{ once: true, amount: 0.1 }}
                   transition={{ delay: index === 0 ? 0.3 : index === 1 ? 0.5 : 0.7, duration: 1 }}
                 >
-                  <Card className={"flex flex-col items-center h-[290px]"}>
+                  <Card className={"flex flex-col items-center md:h-[290px]"}>
                   <div className="bg-blue-gradient w-[60px] h-[60px] rounded-full flex justify-center items-center">
                     <elem.img size={40} className="text-background "/>
                   </div>
