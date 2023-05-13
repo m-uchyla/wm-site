5d89f69f2b2cd70b637fa2225da585632d55ce8a
witekZWitkowic <witoldadamus94@gmail.com>
2023-05-13 17:11:21 +0200

    Conditional rendering for small/large pages

--- src/components/organisms/how.js
+++ src/components/organisms/how.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { motion, useTransform, useSpring, useScroll } from "framer-motion"
+import { motion, useTransform, useSpring, useScroll, useMotionTemplate } from "framer-motion"
 import {howItsDone} from "../../constants/index"
 import Image from 'next/image'
 import { large, small } from '../../constants/deviceAnimation'
@@ -9,16 +9,22 @@ export default function How () {
 
 //screen width 1440 and above -> false; below 1440 -> true
 const [deviceAnimation,setDeviceAnimation] = useState((typeof window !== 'undefined') ? ((window.innerWidth >= 1440) ? large : small) : small);
+const [translateState, setTranslateState] = useState(false);
+
 
 useEffect(() => {
     window.addEventListener('resize', function() {
       if(window.innerWidth >= 1440){
         setDeviceAnimation(large)
+        setTranslateState(true)
       }else{
         setDeviceAnimation(small)
+        setTranslateState(false)
       };
     });
   },[deviceAnimation])
+
+
 
     const translate1 = useTransform(scrollYProgress, deviceAnimation.translate1[0], deviceAnimation.translate1[1])
     const translate2 = useTransform(scrollYProgress, deviceAnimation.translate2[0], deviceAnimation.translate2[1])
@@ -31,12 +37,11 @@ useEffect(() => {
     const yRange = useTransform(scrollYProgress, deviceAnimation.yRange[0], deviceAnimation.yRange[1]);
     const pathLength = useSpring(yRange, { stiffness: 400, damping: 90 });
 
-
   return (
     <section id="howItsDone" >
         <h2 className='text-white sm:text-[52px] text-[48px] font-semibold text-center mb-4 lg:mb-0'>Jak działamy?</h2>
         <motion.svg
-        className={`absolute lg:right-[50%] md:right-[80%] ss:right-[70%] right-[95%] xl:z-[1] z-[-1]`}
+        className={`absolute xl:right-[50%] lg:right-[81%] md:right-[80%] ss:right-[81%] right-[95%] xl:z-[1] z-[-1]`}
         width="6" height="1356" viewBox="0 0 6 1356"
         xmlns="http://www.w3.org/2000/svg">
             <motion.path 
@@ -51,21 +56,36 @@ useEffect(() => {
             />
         </motion.svg>
         <div className='flex flex-col'>
+       
             {howItsDone.map((how, index) => (
                 <div key={"howItsDone"+index} className={`flex items-center lg:justify-between justify-center ${index % 2 ? "flex-row-reverse" : "flex-row"} ${index === howItsDone.length - 1 ? "mb-0" : "mb-10"}`}>
+                     {/* conditional rendering white large or small */}
+                    {translateState ? 
                     <motion.div
                         style={{
-                            translateY: index === 0 ? translate1 : index === 1 ? translate2 : index === 2 ? translate3 : translate4,
+                            translateX: index === 0 ? translate1 : index === 1 ? translate2 : index === 2 ? translate3 : translate4,
                             opacity: index === 0 ? opacity1 : index === 1 ? opacity2 : index === 2 ? opacity3 : opacity4
                         }}
-                        key={"howItsDone" + index}
-                    >
+                        key={"howItsDone" + index}>
                         <div className='max-w-[510px]'>
                             <h4 className='text-[36px] font-bold'>{how.title}</h4>
                             <div className={`lg:w-[566px] mb-2 ${index % 2 ? "xl:ml-[-63px]" : "ml-0"} h-[5px] bg-[#00D8FF]`}/>
                             <p className='text-[18px] text-secondaryText'>{how.description}</p>
                         </div>
-                    </motion.div>
+                    </motion.div> : 
+                    <motion.div
+                    style={{
+                        translateY: index === 0 ? translate1 : index === 1 ? translate2 : index === 2 ? translate3 : translate4,
+                        opacity: index === 0 ? opacity1 : index === 1 ? opacity2 : index === 2 ? opacity3 : opacity4
+                    }}
+                    key={"howItsDone" + index}>
+                    <div className='max-w-[510px]'>
+                        <h4 className='text-[36px] font-bold'>{how.title}</h4>
+                        <div className={`lg:w-[566px] mb-2 ${index % 2 ? "xl:ml-[-63px]" : "ml-0"} h-[5px] bg-[#00D8FF]`}/>
+                        <p className='text-[18px] text-secondaryText'>{how.description}</p>
+                    </div>
+                </motion.div>
+                }
                     <motion.div 
                     style={{
                         translateX: index === 0 ? translate2 : index === 1 ? translate1 : index === 2 ? translate4 : translate3,
